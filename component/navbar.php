2e1049f5aa3e90cb7191323770c077e9904750ca
Dream Gen <167444274+1ducky@users.noreply.github.com>
2025-09-19 21:51:17 +0700

    new ui navbar

--- component/navbar.php
+++ component/navbar.php
@@ -1,52 +1,62 @@
 <?php if (session_status() === PHP_SESSION_NONE) session_start(); ?>
-<!-- Navbar -->
-<nav class="navbar navbar-expand-lg navbar-custom fixed-top">
-    <div class="container-fluid">
-        <a class="navbar-brand" href="<?= $basepath?>/">
-            <div class="brand-img">GREEN CORE</div>
-        </a>
 
-        <!-- Search Box (desktop) -->
-        <div class="d-none d-lg-flex align-items-center flex-grow-1 mx-lg-4">
-            <div class="input-group">
-                <input type="search" 
-                       name="query" 
-                       id="searchInputDesktop" 
-                       class="form-control search-custom ms-lg-5" 
-                       placeholder="Cari Produk" 
-                       value="<?= ($_GET['keyword']) ?? ''?>"
-                       required>
-                <button class="btn btn-success" id="searchBtnDesktop">
-                    <i class="fas fa-search"></i>
-                </button>
+<nav class="navbar ic ic-shadow navbar-expand-lg fixed-top justify-content-around ">
+    <div class="container-fluid gap-4">
+        <!-- Brand -->
+        <a class="d-flex gap-2" href="<?= $basepath?>/">
+            <img src="<?= $basepath ?>/assets/icon.png" alt="GreenCore" class="icon">
+            <div class="navbar-brand text-white">GreenCore</div>
+        </a>
+        <!-- Center Content -->
+        <div class="flex-fill flex-column gap-2 d-flex text-white d-none d-md-block">
+            <div class=" input px-2 d-flex justify-content-around align-items-center text-white flex-fill rounded-pill sc">
+                <input type="text" name="" id="" class="container-fluid  sc">
+                <button class="border-0 bg-transparent text-white icon "><i class="fas fa-search"></i></button>
+            </div>
+            <div class="link">
+                <ul class="d-flex justify-content-around list-unstyled text-white">
+                    <li class="text-decoration-none">Beranda</li>
+                    |
+                    <li class="text-decoration-none">Produk</li>
+                    |
+                    <li class="text-decoration-none">Keranjang</li>
+                    |
+                    <li class="text-decoration-none">Tentang</li>
+                </ul>
             </div>
         </div>
+        <!-- End Centent -->
+         <div class="text-white d-flex justify-content-md-end justify-content-around flex-md-shrink-1 flex-md-shrink-0 gap-4 gap-md-0">
+             <!-- Mobile Search Box -->
+              <button class="rounded-circle sc d-block d-md-none me-2 icon" id="mobileSearchBtn">
+                 <i class="fas fa-search"></i>
+             </button>
+     
+             <!-- Login User -->
+             <div class="d-flex flex-column justify-content-center align-content-center gap-2 ">
+                 <center>
+                     <img src="<?= $basepath ?>/image/notfound.jpg" alt="GreenCore" class="icon rounded-circle">
+                 </center>
+                 <p class="sc rounded-pill p-1 d-none d-md-block">Login/Sign-Up</p>
+             </div>
+     
+             <!-- Mobile Menu Toggler -->
+              <button class=" sc d-block d-md-none icon rounded-circle" type="button" data-bs-toggle="offcanvas" data-bs-target="#navtoggler" aria-controls="navtoggler">
+                 <i class="fas fa-bars"></i>
+             </button>
 
-        <!-- Search Icon (mobile) -->
-        <button class="btn btn-success d-lg-none me-2" id="mobileSearchBtn">
-            <i class="fas fa-search"></i>
-        </button>
+         </div>
 
-        <!-- Toggler -->
-        <button class="navbar-toggler navbar-toggler-custom" type="button" data-bs-toggle="offcanvas" data-bs-target="#navtoggler" aria-controls="navtoggler">
-            <i class="fas fa-bars"></i>
-        </button>
     </div>
 </nav>
-
 <!-- Search input muncul saat icon ditekan (mobile) -->
-<div id="mobileSearchBox" class="d-lg-none px-3 py-2 bg-light d-none fixed-top z-1 ">
-    <div class="input-group">
-        <input type="text" 
-               name="query" 
-               id="searchInputMobile" 
-               class="form-control" 
-               placeholder="Cari Produk" 
-               value="<?= ($_GET['keyword']) ?? ''?>"
-               required>
-        <button class="btn btn-success" id="searchBtnMobile">
-            <i class="fas fa-search"></i>
+<div id="mobileSearchBox" class="text-white d-lg-none px-3 py-2 bg-light d-none fixed-top z-1 ">
+    <div class="input px-2 d-flex justify-content-around align-items-center text-white flex-fill rounded-pill sc">
+        <button class="rounded-circle sc d-block d-md-none icon z-2" id="mobileSearchBtn">
+            <
         </button>
+        <input type="text" name="" id="searchInputMobile" class="container-fluid  sc">
+        <button class="btn "><i class="fas fa-search"></i></button>
     </div>
 </div>
 
@@ -87,8 +97,6 @@
         </ul>
     </div>
 </div>
-
-
 
 <script>
         $(document).ready(function(){
